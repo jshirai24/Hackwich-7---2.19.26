a4e23acf740b59c9a5376cc242c7b01f23a93ca9
Jillian Shirai <jtshirai@hawaii.edu>
2026-02-20 16:52:59 -1000

    Figured out how to iterate over the array, had to put the + 1 as a seperate thing from the .text code part else it get confused

---
+++ Hackwich 7 - 2.19.26/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Hackwich 7 - 2.19.26
+//
+//  Created by Jillian Shirai on 2/20/26.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
